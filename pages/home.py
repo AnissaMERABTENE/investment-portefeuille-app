0fb8f1526a582d14077f93d24a53618fd9bc7774
Sofiene <sofiene@client-172-18-120-178.eduroam.universite-paris-saclay.fr>
2024-10-25 16:32:27 +0200

    V0.1 formulaire done

--- pages/home.py
+++ pages/home.py
@@ -18,27 +18,4 @@ if st.button("Simuler"):
     taux_rendement = rendement_annuel / 100
     mois = duree * 12
 
-#Créer une série de croissance avec NumPy
-    montants = [montant_initial]
-    for mois in range(1, mois + 1):
-        dernier_montant = montants[-1]
-        dernier_montant += contributions
-        dernier_montant *= (1 + taux_rendement / 12)  # Rendement mensuel
-        montants.append(dernier_montant)
-
-#Afficher les résultats avec Pandas
-    df = pd.DataFrame({
-        'Mois': np.arange(1, len(montants) + 1),
-        'Valeur du Portefeuille (€)': montants
-    })
-
-    st.write("Voici l'évolution du portefeuille au fil du temps")
-    st.dataframe(df)
-
-#Visualiser les résultats avec Matplotlib
-    fig, ax = plt.subplots()
-    ax.plot(df['Mois'], df['Valeur du Portefeuille (€)'])
-    ax.set_xlabel("Mois")
-    ax.set_ylabel("Valeur du Portefeuille (€)")
-    ax.set_title("Croissance du Portefeuille dans le Temps")
-    st.pyplot(fig)
+# 
